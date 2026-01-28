856c285be2fa2dc9f382a3b09d0c6ce66cd7eb65
Angel Rodriguez <ared230000@gmail.com>
2026-01-28 13:53:53 -0600

    Cambiar titulo y subtitulo con botón

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 const estadoUI = $('#estadoUI');
 const setEstado = (msg) => { estadoUI.textContent = msg; };
 setEstado('Listo');
+
+const btnCambiar = $('#btnCambiarMensaje');
+const titulo = $('#tituloPrincipal');
+const subtitulo = $('#subtitulo');
+
+// Manejador del evento click del botón
+btnCambiar.addEventListener('click', () => {
+    const alt = titulo.dataset.alt === '1';
+    titulo.textContent = alt
+        ? 'Haz sido troleado por JS'
+        : 'Bienvenido a la aplicación de EJ';
+
+    subtitulo.textContent = alt
+        ? '¡Sorpresa!'
+        : 'Esto es una aplicación sencilla de manipulacion del DOM';
+
+    titulo.dataset.alt = alt ? '0' : '1';
+    setEstado('Textos actualizados')
+    
+});
