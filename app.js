88b9554eb6d1c5e77284edfac8d30aa8d4951a04
Angel Rodriguez <ared230000@gmail.com>
2026-02-09 13:58:58 -0600

    Delegación del evento click para botón like

--- app.js
+++ app.js
@@ -93,14 +93,25 @@ btnLimpiar.addEventListener('click', () => {
     setEstado(`Se eliminaron ${removed} cards`);
 });
 
-const likeButtons = document.querySelectorAll('#listaArticulos button[data-action="like"]')
+// const likeButtons = document.querySelectorAll('#listaArticulos button[data-action="like"]')
 
-likeButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const card = btn.closest('.card');
-        hacerLike(card)
-    });
+const listaArticulos3 = $('#listaArticulos');
+
+listaArticulos3.addEventListener('click', (e) => {
+    // ¿Se hizo click en un boton de likeS?
+    const btn = e.target.closest('button[data-action="like"]');
+    if (!btn) return; // No es un botón de like, salir
+    const card = btn.closest('.card');
+    if (!card) return; // No se encontró la card, salir
+    hacerLike(card);
 });
+
+// likeButtons.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         const card = btn.closest('.card');
+//         hacerLike(card)
+//     });
+// });
 
 const hacerLike = (card) => {
     const badge = card.querySelector('.badge');
